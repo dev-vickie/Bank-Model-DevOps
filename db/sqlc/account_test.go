06d4124c73a9afc80b7a6ed7f36bfca614c2ff88
dev-vickie <vmutethia84@gmail.com>
2024-03-12 19:50:27 +0300

    test CreateAccount method

---
+++ db/sqlc/account_test.go
@@ -0,0 +1,23 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateAccount(t *testing.T) {
+	arg := CreateAccountParams{
+		Owner:    "Victor",
+		Balance:  50,
+		Currency: "KES",
+	}
+	account, err := testQueries.CreateAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, account)
+
+	require.Equal(t, arg.Owner, account.Owner)
+	require.Equal(t, arg.Currency, account.Currency)
+	require.Equal(t, arg.Balance, account.Balance)
+}
